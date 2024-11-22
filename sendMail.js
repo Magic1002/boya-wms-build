42c7a08af64ce490bc4bf932353b566eb9164b92
c-z-f <18295790529@163.com>
2024-11-22 21:40:20 +0800

    修改sendMail.js

--- sendMail.js
+++ sendMail.js
@@ -15,22 +15,29 @@ const projectNamePrompt = [
     value: 'WMSdev'
   },
   {
-    name: '精益生产物料管理系统(仓储1.7服务器_https ip:192.168.1.7)',
+    name: '精益生产物料管理系统(仓储1.5服务器_https ip:192.168.1.5)',
     value: 'WMSdevSecure'
+  },
+  {
+    name: '精益生产物料管理系统(仓储内网域名服务器_https https://intranet.byimt.com)',
+    value: 'WMSdevIntranet'
   }
 ]
 
 const transporter = nodemailer.createTransport({
   // host: 'smtp.ethereal.email',
-  service: 'qq', // 使用了内置传输发送邮件 查看支持列表：https://nodemailer.com/smtp/well-known/
-  port: 465, // SMTP 端口
+  // service: '腾讯企业邮箱', // 选择你的邮件服务提供商，例如腾讯企业邮箱、网易邮箱等
+  // service: 'qq', // 使用了内置传输发送邮件 查看支持列表：https://nodemailer.com/smtp/well-known/
+  service: '163', // 使用了内置传输发送邮件 查看支持列表：https://nodemailer.com/smtp/well-known/
+  port: 465, // SMTP 端口(邮件传输协议)
   secureConnection: true, // 使用了 SSL
   auth: {
-    user: '773983210@qq.com',
-    // user: '18295790529@163.com',
+    // user: '773983210@qq.com',
+    user: '18295790529@163.com', // 发件人邮箱
     // 这里密码不是qq密码，是你设置的smtp授权码
-    pass: 'foxyxamgunidbebf'
-    // pass: 'rrcqwtbjexlibadd'
+    // pass: 'foxyxamgunidbebf'
+    pass: 'rrcqwtbjexlibadd' // 发件人邮箱密码或授权码
+    // pass: '163Email134658' // 发件人邮箱密码或授权码
   }
 })
 
@@ -70,10 +77,13 @@ function sendMail(message, projectName, zipName) {
       chooseProjectName = i.name
     }
   })
+  // 邮件选项
   const mailOptions = {
-    from: '"773983210" <773983210@qq.com>', // 发送地址
-    to: projectName.indexOf('dev') !== -1 ? maillistDev : maillistProd, // 接收者
-    subject: `${customerName}打包文件`, // 主题
+    // from: '"773983210" <773983210@qq.com>', // 发件人邮箱
+    from: '18295790529@163.com', // 发件人邮箱
+    to: projectName.indexOf('dev') !== -1 ? maillistDev : maillistProd, // 收件人邮箱
+    subject: `${customerName}打包文件`, // 邮件主题
+    // 邮件正文（HTML）
     html: `<h4>Dear:</h4>
         <p h5 style="display: inline-block;text-indent: 2em;">附件是 <h5 style="display: inline;"> ${customerName} (${projectName.indexOf('dev') !== -1 ? '测试环境' : '正式环境'})</h5> （${chooseProjectName}）前端部署打包文件。</p>
         <p style="text-indent: 2em;">本次打包${message.installTime[0]}</p>
@@ -83,12 +93,13 @@ function sendMail(message, projectName, zipName) {
         <h5>Good luck</h5><h4 style="text-indent:35em;">博雅智能</h4>`, // 内容主体
     attachments: [
       {
-        filename: `${customerName}${moment().format('YYYY-MM-DD')}.tar.gz`,
-        path: zipFile
+        filename: `${customerName}${moment().format('YYYY-MM-DD')}.tar.gz`, // 附件文件名
+        path: zipFile // 附件文件路径
       }
     ]
   }
   // send mail with defined transport object
+  // 发送邮件
   transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
       return console.log(error)
